bb7ed3cb15da8281db5776fd28cd54f243c16557
Teerapong Singthong <teerapong.singthong@krungthai-axa.co.th>
2018-04-05 12:21:01 +0700

    Add PUT method

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const sequelize = require('./config').sequelize;
+const Op = sequelize.Op;
 const Todo = sequelize.import('./models/todo');
 const TransactionOptions = require('./config').transactionOptions;
 
@@ -25,12 +26,50 @@ app.get('/todos', async (req, res, next) => {
   res.json(result);
 });
 
-app.post('/todos', (req, res, next) => {
+app.post('/todo', async (req, res, next) => {
+  try {
+    let todo = req.body;
+    let result = await sequelize.transaction(TransactionOptions, (t) => {
+      return Todo.create(todo, {
+        transaction: t
+      });
+    });
 
+    res.json({
+      message: 'Create todo success',
+      data: result
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: 'Create todo failed'
+    });
+  }
 });
 
-app.put('/todo/:id', (req, res, next) => {
+app.put('/todo/:id', async (req, res, next) => {
+  try {
+    let todoId = parseInt(req.params.id, 10);
+    let newTodo = req.body;
 
+    let result = await sequelize.transaction(TransactionOptions, (t) => {
+      return Todo.update({
+        newTodo
+      }, {
+        where: {
+          [Op.and]: { id: todoId }
+        }
+      });
+    });
+
+    res.json({
+      message: 'Update todo success',
+      data: result
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: 'Update todo failed'
+    });
+  }
 });
 
 app.delete('/todo/:id', (req, res, next) => {
